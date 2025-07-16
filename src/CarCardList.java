1e9fd487f04a84ef8847387dc6dfb52c313e831f
Rehman Kalawant <103535528+rehmankalawant@users.noreply.github.com>
2025-07-16 23:57:17 +0530

    Second Commit After Fail

---
+++ src/CarCardList.java
@@ -0,0 +1,15 @@
+package com.gearshift.carapi.model;
+
+import java.util.List;
+
+public class CarCardList {
+    private List<Car> carCard;
+
+    public List<Car> getCarCard() {
+        return carCard;
+    }
+
+    public void setCarCard(List<Car> carCard) {
+        this.carCard = carCard;
+    }
+}
